323f2552e82214cf806b285b1a60feba23567bbf
Felix Favre <1favre@informatik.uni-hamburg.de>
2013-11-17 16:41:47 +0100

    never commit broken code :)

--- app.js
+++ app.js
@@ -71,19 +71,7 @@ var jobs = [
 ];
 
 io.sockets.on('connection', function (socket) {
-mongo.connect('mongodb://127.0.0.1:27017/angel-hub', function(err, db) {
-    if(err) throw err;
 
-    var collection = db.collection('jobs');
-
-      // Locate all the entries using find
-      collection.find().toArray(function(err, results) {
-        console.dir(results);
-        // Let's close the db
-        db.close();
-      });
-    });
-  });
   socket.emit('dataUpdate', { jobList: jobs });
 
   socket.on('myOtherEvent', function(data) {

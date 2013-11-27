ef56c83656a0c47be1a9154f8e1b485c5d41f61a
Julius Zeidler <julius@zeidlos.de>
2013-11-27 01:40:06 +0100

    Make JSHint shut up.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var express = require('express')
-  , app = express()
-  , server = require('http').createServer(app)
+var express = require('express'),
+  app = express(),
+  server = require('http').createServer(app),
   // , mongo = require('mongodb').MongoClient
-  , format = require('util').format
-  , io = require('socket.io').listen(server)
+  format = require('util').format,
+  io = require('socket.io').listen(server);
 
 app.use("/static", express.static(__dirname + '/static'));
 app.use(express.cookieParser());
